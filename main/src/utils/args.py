35c70a72dc6ec8d6b130c380186216194dfd1ec3
OsamaAhmad369 <83537585+OsamaAhmad369@users.noreply.github.com>
2024-09-01 12:36:40 +0500

    Create args.py

---
+++ main/src/utils/args.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def get_public_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device",type=str,default='')
+    parser.add_argument("--nodes", type=int, default=64, help="Number of nodes for superpixels")
+    parser.add_argument("--epochs", type=int, default=200)
+    parser.add_argument("--bs", type=int, default=8)
+    parser.add_argument("--penalty", type=float, default=1e-4)
+    parser.add_argument("--lrate", type=float, default=1e-4)
+    parser.add_argument("--mode",type=str,default="train",help="For evaluation set mode test otherwise train")
+    parser.add_argument("--weightspath",type=str,default='')
+    return parser
